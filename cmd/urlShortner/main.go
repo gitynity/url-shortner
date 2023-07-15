31cf1d8c43ea383d511776bfd1ee099f55e9d696
Nityanand Sharma <nityanand.official@gmail.com>
2023-07-16 05:05:34 +0530

    Create main.go

---
+++ cmd/urlShortner/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	dblayer "url-shortner/DBLayer"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	db, err := dblayer.DBconfig()
+	if err != nil {
+		fmt.Println("Getting DB Connection", err)
+		os.Exit(3)
+	}
+	r := mux.NewRouter()
+	handlers := registerHandlers(db)
+
+	r.Handle("/get-short-url", handlers["get-short-url"]).Methods(http.MethodGet)
+
+	r.Handle("/get-long-url", handlers["get-long-url"]).Methods(http.MethodGet)
+
+	r.Handle("/remove-url", handlers["remove-url"]).Methods(http.MethodDelete)
+
+	r.Handle("/add-url", handlers["add-url"]).Methods(http.MethodPost)
+
+	err = http.ListenAndServe("localhost:8080", r)
+	if err != nil {
+		os.Exit(4)
+	}
+
+	fmt.Println("Server Started")
+}
